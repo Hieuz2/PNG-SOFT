502e32422e9eab8d42834e8ec426aaf74b748ccd
TienDung1605 <TienDung160500>
2023-12-27 19:12:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
+
+function toggleIcon() {
+    var icon = document.getElementById("toggleIcon");
+
+    
+    if (icon.src.endsWith("./icons/toggle-on-solid.svg")) {
+        icon.src = "./icons/toggle-off-solid.svg";
+    } else {
+        icon.src = "./icons/toggle-on-solid.svg";
+    }
+}
